5dfecc704a602534542a34b89cf18bdf0afa94c4
GIKExe <i.gortolomei@yandex.ru>
2025-07-08 10:38:51 +0300

    запись в png (успех)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use std::{
-	fs,
-	io::{BufWriter, Write},
-	panic,
-};
-
+use std::{fs, io::Write, panic};
 use clap::{ArgAction, Parser};
-use jpeg_decoder::{ColorTransform, Decoder};
-use jpeg_encoder::{ColorType, Encoder};
+use image::{ImageReader, Rgb};
 
 #[derive(Parser)]
 #[command(
@@ -42,70 +36,85 @@ fn get_z(num: usize) -> String {
 	}
 }
 
-fn write_bits_to_pixels(pixels: &mut [u8], data: &[u8]) {
+type Pixel = (u8, u8, u8);
+
+fn write_bits_to_pixels(pixels: &mut Vec<Pixel>, data: &[u8]) {
 	let mut data = data.iter();
 	let mut sh = 0u8;
 	let mut byte = *data.next().unwrap_or(&0);
-	for chunk in pixels.chunks_exact_mut(3) {
-		// chunk = [Y, Cb, Cr]
-		chunk[0] = (chunk[0] & 252) | ((byte >> sh) & 3);
-		sh += 2;
-		if sh > 7 {
-			byte = *data.next().unwrap_or(&0);
-			sh = 0
-		};
+	for (r, g, b) in pixels {
+		*r = (*r & 252) | ((byte >> sh) & 3); sh += 2;
+		if sh > 7 { byte = *data.next().unwrap_or(&0); sh = 0 };
+
+		*g = (*g & 252) | ((byte >> sh) & 3); sh += 2;
+		if sh > 7 { byte = *data.next().unwrap_or(&0); sh = 0 };
+
+		*b = (*b & 252) | ((byte >> sh) & 3); sh += 2;
+		if sh > 7 { byte = *data.next().unwrap_or(&0); sh = 0 };
 	}
 }
 
-fn read_bits_from_pixels(pixels: &[u8]) -> Vec<u8> {
+fn read_bits_from_pixels(pixels: &Vec<Pixel>) -> Vec<u8> {
 	let mut data = Vec::new();
 	let mut sh = 0u8;
 	let mut byte = 0;
-	for chunk in pixels.chunks_exact(3) {
-		// chunk = [Y, Cb, Cr]
-		byte |= (chunk[0] & 3) << sh;
-		sh += 2;
-		if sh > 7 {
-			data.push(byte);
-			byte = 0;
-			sh = 0
-		}
+	for (r, g, b) in pixels {
+		byte |= (*r & 3) << sh; sh += 2;
+		if sh > 7 { data.push(byte); byte = 0; sh = 0 }
+
+		byte |= (*g & 3) << sh; sh += 2;
+		if sh > 7 { data.push(byte); byte = 0; sh = 0 }
+
+		byte |= (*b & 3) << sh; sh += 2;
+		if sh > 7 { data.push(byte); byte = 0; sh = 0 }
 	}
 	data.push(byte);
 	data
 }
 
+mod default {
+	pub const INPUT: &str = "input.png";
+	pub const OUTPUT: &str = "output.png";
+	pub const DATA: &str = "input.zip";
+}
+
 fn process() {
 	let args = Args::parse();
 
-	let path = args.input.unwrap_or("input.jpg".to_string());
+	let path = args.input.unwrap_or(default::INPUT.to_string());
 	println!("Чтение {}", &path);
-	let file = fs::File::open(&path).unwrap_or_else(|_| panic!("Не удалось открыть {}", &path));
-	let mut decoder = Decoder::new(file);
-	decoder.set_color_transform(ColorTransform::YCbCr);
-	let mut pixels = decoder.decode().expect("Не удалось декодировать");
-	let metadata = decoder.info().expect("Не удалось получить метаданные");
+	let mut image = ImageReader::open(&path)
+		.unwrap_or_else(|_| panic!("Не удалось открыть {}", &path))
+		.decode()
+		.expect("Не удалось декодировать изображение")
+		.into_rgb8();
+
+	let mut pixels: Vec<(u8, u8, u8)> = image.pixels()
+		.map(|p| (p[0], p[1], p[2])).collect();
 
 	if args.read {
 		// Чтение файла из фото
 		println!("Читаем файл из фото...");
-		let data = read_bits_from_pixels(&pixels);
+		let mut data = read_bits_from_pixels(&pixels);
+		if let Some(index) = data.iter().rposition(|&b| b != 0x00) {
+			data.truncate(index + 1); // Сохраняем последний ненулевой элемент
+		}
 
-		let path = args.output.unwrap_or("output.jpg".to_owned());
-		let mut output_file = fs::File::create(&path)
-			.unwrap_or_else(|_| panic!("Не удалось создать/перезаписать файл: {}", &path));
-		output_file
+		let path = args.output.unwrap_or(default::OUTPUT.to_string());
+		fs::File::create(&path)
+			.unwrap_or_else(|_| panic!("Не удалось создать/перезаписать файл: {}", &path))
 			.write_all(&data)
 			.expect("Не удалось записать данные в файл");
+
 	} else {
 		// Запись файла в фото
 		println!("Всего пикселей: {}", pixels.len());
-		let max_bytes = pixels.len() * 2 / 8;
+		let max_bytes = pixels.len() * 6 / 8;
 		println!("Доступно для записи: {}\n", get_z(max_bytes));
 
-		let path = args.data.unwrap_or("input.7z".to_owned());
+		let path = args.data.unwrap_or(default::DATA.to_string());
 		println!("Чтение {}", &path);
-		let data = fs::read("input.7z").unwrap_or_else(|_| panic!("Не удалось открыть {}", &path));
+		let data = fs::read(&path).unwrap_or_else(|_| panic!("Не удалось открыть {}", &path));
 		println!("Размер файла: {}", get_z(data.len()));
 
 		if data.len() > max_bytes {
@@ -116,39 +125,20 @@ fn process() {
 		write_bits_to_pixels(&mut pixels, &data);
 
 		println!("Обратное преобразование");
-		let path = args.output.unwrap_or("output.jpg".to_owned());
-		let file =
-			fs::File::create(&path).unwrap_or_else(|_| panic!("Не удалось записать файл: {}", &path));
-		let writer = BufWriter::new(file);
-		let encoder = Encoder::new(writer, 100);
-		encoder
-			.encode(&pixels, metadata.width, metadata.height, ColorType::Ycbcr)
-			.expect("Ошибка кодирования");
+		let (width, _height) = image.dimensions();
+		for (x, y, pixel) in image.enumerate_pixels_mut() {
+			let index = (y * width + x) as usize;
+			let (r, g, b) = pixels[index];
+			*pixel = Rgb([r, g, b]);
+		}
 
-		// let (width, height) = rgb_img.dimensions();
-		// let mut output_img: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(width, height);
-
-		// for (x, y, pixel) in output_img.enumerate_pixels_mut() {
-		// 	let index = (y * width + x) as usize;
-		// 	let (r, g, b) = pixels[index];
-		// 	*pixel = Rgb([r, g, b]);
-		// }
-
-		// // Сохранение в JPEG
-		// let path = args.output.unwrap_or("output.jpg".to_owned());
-		// let mut output_file = fs::File::create(&path)
-		// 	.unwrap_or_else(|_| panic!("Не удалось записать файл: {}", &path));
-		// let mut encoder = JpegEncoder::new_with_quality(&mut output_file, 100);
-		// encoder
-		// 	.encode_image(&output_img)
-		// 	.expect("Не удалось закодировать изображение");
+		let path = args.output.unwrap_or(default::OUTPUT.to_string());
+		image.save(&path).unwrap_or_else(|_| panic!("Не удалось сохранить изображение: {}", &path));
 	}
 }
 
 fn main() {
-	let result = panic::catch_unwind(|| {
-		process();
-	});
+	let result = panic::catch_unwind(|| {process()});
 	match result {
 		Ok(_) => println!("Программа завершилась без ошибок\n\n "),
 		Err(e) => println!("{e:?}"),
--- test.cmd
+++ test.cmd
@@ -1,3 +1,3 @@
 @echo off
-secret -i input.png -d input.7z -o output.png
-secret -r -i output.png -o output.7z
+secret -i ../input.png -d ../input.zip -o output.png
+secret -r -i output.png -o output.zip
